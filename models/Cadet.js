a9368cbd6c92b079825424a07832d053e785b703
BeathovenGala <has200613@gmail.com>
2025-08-26 15:44:32 +0530

    hashing pass before save

--- models/Cadet.js
+++ models/Cadet.js
@@ -2,7 +2,7 @@
 //steps 1(import),2(schema),3(create and export)
 //import mongoose
 const mongoose = require('mongoose');
-
+const bycrypt =require('bcryptjs');
 //Define the schema for our Cadet data
 // A schema defines the structure, data types, and validation rules for our documents.
 const CadetSchema=new mongoose.Schema({
@@ -48,7 +48,31 @@ const CadetSchema=new mongoose.Schema({
 
 });
 
-const Cadet=mongoose.model(`Cadet`,`CadetSchema`);// 3. Create and export the Mongoose model
+//we will save the password directly into hash before saving to db   i.e pre save
+CadetSchema.pre('save',async function(next) { // We use a pre-save hook to ensure the password is always hashed.
+   //it is a gaurd clause so we don't need if else it imediately moves to the next if  condition is met
+    // Only hash the password if it has been modified(aka inserted) ,,,or if new or else hook won't be called
+    if(!this.isModified('password')){//this hook says is password is NOT modified then move on to next step
+     return next();
+    }    
+    //if it is modified(aka new inserted)
+    try {
+         // Generate a salt. A salt is a random string added to the password
+        // to make the hash unique, even for the same password.
+        const salt = await bycrypt.genSalt(10);
+        this.password = await bycrypt.hash(this.password,salt);
+        next();
+    } catch (err) {
+        next(err);//send s error to the try catch of index.js
+        
+    }
+});
+
+
+
+
+//mongoose.model('name_of_model','schema')
+const Cadet=mongoose.model('Cadet',CadetSchema);// 3. Create and export the Mongoose model
 // This model provides us with functions to interact with the database (e.g., save(), find()).
 
 module.exports =Cadet; //it like a library. The Cadet class is a book in that library. module.exports = Cadet; puts that book on the main shelf, so anyone who comes into the library can check it out.
