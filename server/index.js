9c98a7796550ce983970d4a97a8fc4c7a57f6318
BeathovenGala <has200613@gmail.com>
2025-08-27 19:41:23 +0530

    utils token and auth

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const mongo_url='mongodb://localhost:27017/cadet-tracker';
 const app=express();    //our server instance
 const Cadet=require('C:\\Users\\nseg.lcl\\Documents\\CadetTracker\\models\\Cadet.js');
 const jwt=require('jsonwebtoken');
-
+const { protect } = require('C:\\Users\\nseg.lcl\\Documents\\CadetTracker\\Utils\\authMiddleware.js');
 
 app.use(express.json());//middleware to handle json data
 
@@ -19,6 +19,7 @@ const JWT_SECRET = 'secret_super_key' ;
 // app.get is receptionist 
 // app variable represents the entire app
 // get(path,handler) 
+
 app.get('/',(req,res)=>{ //here the handler says  a route handler's job is to take the information it finds in the req object and use it to craft an appropriate reply with the res object.
     res.send(`welcome to cadetTracker Backend`);
 });
@@ -42,12 +43,7 @@ app.post('/login',async(req,res)=>{
              if(!isMatch){   // If the passwords don't match, send an unauthorized response and exit.
                 return res.status(401).json({message: 'Incorrect password'});
                }
-               const token= jwt.sign(//JWT consists of three parts, separated by dots (.) once they are converted       //E.G. ---- Formula:HMACSHA256(base64UrlEncode(header) + "." + base64UrlEncode(payload), secret) The final JWT is the concatenation of these three encoded parts, separated by periods: [Header].[Payload].[Signature]
-                ////Header: Contains the token type (JWT) and the signing algorithm //Payload: Contains the "claims" or statements about an entity (like a user) and additional data. Claims can include the user ID, username, and an expiration time. //Signature: Created by taking the encoded header, the encoded payload, a secret key, and the algorithm specified in the header. The server uses this signature to verify that the token hasn't been tampered with
-                {id: cadet._id}, //Payload
-                JWT_SECRET, //Secret Key Only  server should know this key
-                {expiresIn: '1h'} //Options,,, configure the token's behavior
-               );
+                const token = jwt.sign({ id: cadet._id }, JWT_SECRET, { expiresIn: '1h' });
               // If the cadet is found and the password is correct, send a success message.
                     res.status(200).json({ message: 'Login successful!', token });//send token back to clinet
         }
