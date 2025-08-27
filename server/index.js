0853eca2e8d422b6f587e656106be49e0771ccc1
BeathovenGala <has200613@gmail.com>
2025-08-27 18:05:14 +0530

    JWT to login routes

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const mongoose=require('mongoose');//import mongoose e.g. translator for monogDB
 const mongo_url='mongodb://localhost:27017/cadet-tracker';
 const app=express();    //our server instance
 const Cadet=require('C:\\Users\\nseg.lcl\\Documents\\CadetTracker\\models\\Cadet.js');
+const jwt=require('jsonwebtoken');
+
 
 app.use(express.json());//middleware to handle json data
 
 const PORT=process.env.PORT || 3000; //declaring constant variable named PORT and process.env is an object that holds users envlironment variables
                                     //Env variables--what it runs on--The program will automatically use that value.
                                    // we then or that so if no env variables we set default port
+const JWT_SECRET = 'secret_super_key' ;
 ////Route for url
 // app.get is receptionist 
 // app variable represents the entire app
@@ -21,6 +24,7 @@ app.get('/',(req,res)=>{ //here the handler says  a route handler's job is to ta
 });
 
 //route for login
+//now generate jwt upon succesful login
 app.post('/login',async(req,res)=>{
      //get data from frontend
     try {
@@ -38,8 +42,14 @@ app.post('/login',async(req,res)=>{
              if(!isMatch){   // If the passwords don't match, send an unauthorized response and exit.
                 return res.status(401).json({message: 'Incorrect password'});
                }
+               const token= jwt.sign(//JWT consists of three parts, separated by dots (.) once they are converted       //E.G. ---- Formula:HMACSHA256(base64UrlEncode(header) + "." + base64UrlEncode(payload), secret) The final JWT is the concatenation of these three encoded parts, separated by periods: [Header].[Payload].[Signature]
+                ////Header: Contains the token type (JWT) and the signing algorithm //Payload: Contains the "claims" or statements about an entity (like a user) and additional data. Claims can include the user ID, username, and an expiration time. //Signature: Created by taking the encoded header, the encoded payload, a secret key, and the algorithm specified in the header. The server uses this signature to verify that the token hasn't been tampered with
+                {id: cadet._id}, //Payload
+                JWT_SECRET, //Secret Key Only  server should know this key
+                {expiresIn: '1h'} //Options,,, configure the token's behavior
+               );
               // If the cadet is found and the password is correct, send a success message.
-                    res.status(200).json({ message: 'Login successful!', cadet });
+                    res.status(200).json({ message: 'Login successful!', token });//send token back to clinet
         }
      catch (err) {
         //to handle errors that occur while looking for data within the server
@@ -56,9 +66,10 @@ app.post('/register',async(req,res)=>{
     try {
         //get cadet data
         const cadetData= req.body;
-        const newCadet= new Cadet(cadetData);
+        const newCadet= new Cadet(cadetData); // Create a new instance of our Cadet model. The pre-save hook in the model
+        // will automatically hash the password before saving.
         await newCadet.save();
-        console.log('new cadet is reigstered');  
+        console.log('New cadet is registered');  
         res.status(201).json({message: `Registration rcvd`,
                               cadet: newCadet,
         });//future we'll verify via email
